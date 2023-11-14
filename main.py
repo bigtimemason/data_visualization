2a6636c5031e1d60d88aa04fcf7b8683f86cbeb2
Mason Miller <mhmiller04@gmail.com>
2023-11-13 22:52:00 -0500

    formatting plot

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 year = [1950, 1970, 1990, 2010]
 pop = [2.519, 3.692, 5.263, 6.972]
 
-plt.plot(year, pop)
+plt.scatter(year, pop)
+
+# formatting the plot
+plt.title('Population (Billions) vs Year')
+plt.xlabel('Year')
+plt.ylabel('Population (Billions)')
+
 plt.show()
